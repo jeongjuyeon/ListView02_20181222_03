f5aded12d51ed62252eda043cd7e795a3da78005
HanulGye <kyehl@naver.com>
2018-12-22 15:08:06 +0900

    임시 데이터 삽입 코드

--- app/src/main/java/kr/tjeit/listview02_20181222_03/MainActivity.java
+++ app/src/main/java/kr/tjeit/listview02_20181222_03/MainActivity.java
@@ -31,8 +31,22 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void setValues() {
-
+        fillStores();
     }
+
+    //    가게 데이터를 채우기 위함 임시 메쏘드
+    void fillStores() {
+        storeList.clear();
+
+        storeList.add(new Store("BBQ", "종로구 10길", 4, "오전 9:00 ~ 오후 9:00", "010123456789"));
+        storeList.add(new Store("BBQ1", "종로구 9길", 5, "오전 9:00 ~ 오후 9:00", "146514561153"));
+        storeList.add(new Store("BBQ2", "종로구 8길", 6, "오전 9:00 ~ 오후 9:00", "0113212313213"));
+        storeList.add(new Store("BBQ3", "종로구 7길", 7, "오전 9:00 ~ 오후 9:00", "010123456789"));
+        storeList.add(new Store("BBQ4", "종로구 6길", 8, "오전 9:00 ~ 오후 9:00", "010123456789"));
+        storeList.add(new Store("BBQ5", "종로구 5길", 9, "오전 9:00 ~ 오후 9:00", "010123456789"));
+        storeList.add(new Store("BBQ6", "종로구 4길", 10, "오전 9:00 ~ 오후 9:00", "010123456789"));
+    }
+
 
     @Override
     public void bindViews() {
--- app/src/main/java/kr/tjeit/listview02_20181222_03/adapters/StoreAdapter.java
+++ app/src/main/java/kr/tjeit/listview02_20181222_03/adapters/StoreAdapter.java
@@ -2,7 +2,12 @@ package kr.tjeit.listview02_20181222_03.adapters;
 
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -15,7 +20,7 @@ public class StoreAdapter extends ArrayAdapter<Store> {
     List<Store> mList;
     LayoutInflater inf;
 
-    public StoreAdapter (Context mContext, List<Store> mList) {
+    public StoreAdapter(Context mContext, List<Store> mList) {
         super(mContext, R.layout.store_list_item, mList);
 
         this.mContext = mContext;
@@ -23,4 +28,33 @@ public class StoreAdapter extends ArrayAdapter<Store> {
         this.inf = LayoutInflater.from(mContext);
     }
 
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View row = convertView;
+        if (row == null) {
+            row = inf.inflate(R.layout.store_list_item, null);
+        }
+
+        TextView storeNameTxt = row.findViewById(R.id.storeNameTxt);
+        TextView storeAddressTxt = row.findViewById(R.id.storeAddressTxt);
+        Button storeCallBtn = row.findViewById(R.id.storeCallBtn);
+        TextView storeOpenTimeTxt = row.findViewById(R.id.storeOpenTimeTxt);
+
+        final Store data = mList.get(position);
+
+        storeNameTxt.setText(data.getName());
+        storeAddressTxt.setText(data.getAddress());
+        storeOpenTimeTxt.setText(data.getOpenAndCloseTime());
+
+        storeCallBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(mContext, data.getPhoneNum()+"에게 전화를 겁니다.", Toast.LENGTH_SHORT).show();
+
+
+            }
+        });
+
+        return row;
+    }
 }
