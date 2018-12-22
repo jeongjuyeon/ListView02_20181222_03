dd397200fb8925202f01a85cd328c12883fdbd32
HanulGye <kyehl@naver.com>
2018-12-22 13:41:22 +0900

    Initial commit

---
+++ app/src/main/java/kr/tjeit/listview02_20181222_03/MainActivity.java
@@ -0,0 +1,17 @@
+package kr.tjeit.listview02_20181222_03;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bindViews();
+        setupEvents();
+        setValues();
+    }
+}
