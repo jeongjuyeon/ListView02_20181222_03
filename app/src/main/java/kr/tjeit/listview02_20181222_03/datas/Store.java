f1e45279bb1aa3fdf7ff869be8c8eed97041af7c
HanulGye <kyehl@naver.com>
2018-12-22 16:12:53 +0900

    메인액티비의 리스트뷰를 누른 줄에 따라 가게 상세화면의 내용이 다르게 뜨도록

--- app/src/main/java/kr/tjeit/listview02_20181222_03/datas/Store.java
+++ app/src/main/java/kr/tjeit/listview02_20181222_03/datas/Store.java
@@ -1,6 +1,8 @@
 package kr.tjeit.listview02_20181222_03.datas;
 
-public class Store {
+import java.io.Serializable;
+
+public class Store implements Serializable {
 
 //    클래스 : 변수 + 메쏘드 (+ 생성자) 의 조합.
 
